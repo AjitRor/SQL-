c78f5638c7c8a7e160ca048ac8696edc757a4621
AjitRor <ajitturan99@gmail.com>
2025-10-21 15:45:39 +0530

    Add files via upload

---
+++ insert, update, delete.sql
@@ -0,0 +1,18 @@
+-- select * from customer;
+-- insert into customer (customer_id, name , email)
+-- values(1,'Ajit','sawanturan2019@gmail.com');
+-- select * from customer;
+-- insert into customer(customer_id, name, email)
+-- values(2,'Amit',null);
+-- select * from customer;
+-- update customer
+-- set name = 'Shrusthi'
+-- where customer_id = 2;
+-- select * from customer;
+-- insert into customer (customer_id, name, email)
+-- values(3,'Manish', Null);
+-- select * from customer;
+-- delete from customer
+-- where customer_id = 3;
+select * from customer;
+
